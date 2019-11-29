08e422abf95d309721eaeb753369d695690da465
fujinjin <524160845@qq.com>
2019-11-29 19:14:21 +0800

    11

--- 商城排序/js/index.js
+++ 商城排序/js/index.js
@@ -14,8 +14,9 @@ let cardbox = document.getElementById('cardbox')
 let xhr = new XMLHttpRequest //创建一个实例
 xhr.open('GET', 'json/product.json', false);//打开一个请求链接，基于get请求 同步
 xhr.onreadystatechange = function () {
-    //如果状态是200，而且状态码是4
+    //如果状态是200，而且状态码是4，证明数据请求成功
     if (xhr.status === 200 && xhr.readyState === 4) {
+        //把请求来的数据赋值给变量接受
         data = xhr.responseText
     }
 }
@@ -63,7 +64,7 @@ for (var i = 0; i < navlist.length; i++) {
     navlist[i].index = i //给每一按钮设置自定义属性，存储的是当前的索引
     navlist[i].onclick = function () {
         //此方法里的this就是你当前点击的元素
-        this.flag *= -1
+        this.flag *= -1//每点击一次就把当前元素身上的falg乘等于-1
         sortlist.call(this)
         clearArrow.call(this)
         addArrow.call(this)
@@ -75,21 +76,31 @@ for (var i = 0; i < navlist.length; i++) {
 function sortlist() {
     //这里的this就是当前点击的元素
     let dataAry = ['data-time', 'data-hot', 'data-price']
-    //属性映射表，通过当前点击按钮
+    //属性映射表，通过当前点击按钮的索引，拿到映射表中对应的属性名
     cardlist.sort((a, b) => {
         //这里的this就是当前点击的元素（a标签）
         console.log(this.flag)
-        //当我们拿到数据进行页面渲染时，可以把我们想要用的
+        //当我们拿到数据进行页面渲染时，可以把我们想要用的数据绑定到标签的结构上
+        //1.先通过按钮的索引拿到进行排序的属性名
+        //2.然后根据属性名拿到li结构上对应的属性值
+        //3.把属性值重新赋值给变量a和b进行比较排序
         a = a.getAttribute(dataAry[this.index]);
         b = b.getAttribute(dataAry[this.index]);
+
+        //如果条件成立，说你点击的是时间按钮，时间中带“-”，不能进行相减
         if (this.index === 0) {
+            //把时间的“-”去掉
             a = a.replace(/-/g, '')
             b = b.replace(/-/g, '')
 
         }
+        //flag为1的时候是升序，flag为-1的时候为降序
+        //利用return一个负数就会倒序原理来进行升降序的切换
         return (a - b) * this.flag
     })
     // console.log(cardList)
+
+    //把排好序的数组插入页面中
     for (var i = 0; i < cardlist.length; i++) {
         cardbox.appendChild(cardlist[i])
     }
@@ -114,8 +125,10 @@ function clearArrow() {
     }
 }
 function addArrow() {
-    let up = this.children[0]
-    let down = this.children[1]
+    //this就是当前点击的元素
+    let up = this.children[0] //获取向上的箭头
+    let down = this.children[1] //获取向下的箭头
+    //如果当前按钮的flag大于0.说明现在是升序
     if (this.flag > 0) {
         up.classList.add('bg')
         down.classList.remove('bg')
