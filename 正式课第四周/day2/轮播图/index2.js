3635cab66821b8a8d6d45b16e1304e07afe6758a
fujinjin <524160845@qq.com>
2020-01-07 17:51:14 +0800

    111

--- 正式课第四周/day2/轮播图/index2.js
+++ 正式课第四周/day2/轮播图/index2.js
@@ -11,17 +11,85 @@ xhr.onreadystatechange=function(){
     }
 }
 xhr.send()
+console.log(data);
+
 
 let bindHtml=()=>{
     let divs=``
     let lis=``
     data.map((item,index)=>{
-        divs+=`<div><img src="${item.img}"></div>`
-        lis+=`<li></li>`
+        divs+=`<div><img src="${item.img}"></div>`;
+        lis+=` <li></li>`
+       
 
-   
-    })
-    divs+=
+   })
+   divs+=`<div><img src="${data[0].img}"></div>`
+  wrapper.innerHTML=divs
+  list.innerHTML=lis
+}
+bindHtml()
+
+//实现自动轮播 2000ms滑动一张
+// 让wrapper这个元素向左移动800px;
+let step=0
+let autoMove=()=>{
+    step++
+    if(step===5){
+        wrapper.style.left="0px"
+        step=1
+    }
+    changeTip()
+    utils.animate(wrapper,{left:-800*step},300)
+}
+let timer=setInterval(autoMove,2000)
+
+outer.onmouseover=function(){
+    clearInterval(timer)
+}
+outer.onmouseout=function(){
+    timer=setInterval(autoMove,2000)
+}
+//焦点跟随； 显示哪一张图片，给对应的li加上class名select;   
+let oLis=document.getElementsByTagName('li')
+
+let changeTip=()=>{
+    for(let i=0;i<oLis.length;i++){
+        if(step==i){
+            oLis[i].classList.add('select')
+        }else{
+            oLis[i].classList.remove('select')
+        }
+        if(step===4){
+            oLis[0].classList.add('select')
+        }
+    }
+}
+changeTip()
+
+//点击焦点，图片跟随
+let forcus=()=>{
+    for(let i=0;i<oLis.length;i++){
+         oLis[i].onclick=function(){
+             autoMove(i)
+         }
+    }
 
 
+}
+forcus()
+
+let left=document.getElementById('left')
+let right=document.getElementById('right')
+right.onclick=function(){
+    autoMove()
+}
+left.onclick=function(){
+    step--
+    if(step===-1){
+         // 当step=-1时，让其瞬间回到最后一张，也是第一张上；
+        wrapper.style.left='-3200px'
+        step=3
+    }
+    utils.animate(wrapper,{left:-800*step},300)
+    changeTip()
 }
