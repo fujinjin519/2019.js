d8de2a0835786766c6f7164838cbac2e083bc8aa
fujinjin <524160845@qq.com>
2020-02-22 17:21:31 +0800

    111

--- 正式课第九周重点/day5/8.todo.js
+++ 正式课第九周重点/day5/8.todo.js
@@ -1,6 +1,25 @@
 let vm = new Vue({
     el:"#app",
     data:{
-        task:[{isSelected:false,title:"去爬山"},{isSelected:false,title:"跑步"}]
+        task:[{isSelected:false,title:"去爬山"},{isSelected:false,title:"跑步"}],
+        val:''
+    },
+    methods:{
+        addTodo(){
+             this.task.push({isSelected:false,title:this.val})
+             this.val="";
+        },
+        removeTodo(obj){
+              this.task.filter(item=>item!==obj)
+        },
+        computed:{
+            cout(){
+                return this.task.filter(item=>item.isSelected).length;
+
+            }
+
+        }
+    
+
     }
 })
