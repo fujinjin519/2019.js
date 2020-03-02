f2ec132a98ac772a036119b0c0a8588aa4b2cf86
fujinjin <524160845@qq.com>
2020-03-02 17:11:23 +0800

    111

---
+++ webpack-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="a">
+        {{msg}}
+         <button>点击</button>
+    </div>
+</template>
+<script>
+    //  把每个组件放到一个.vue 文件中；这个文件中包含了该组件的HTML结构，css,以及这个组件的对应的对象信息；
+    // 导出
+    // 利用webpack中的vue-loader第三方插件把后缀名是.vue的文件编译成正常的js文件，供HTML来识别；
+    export default{
+        data(){
+            return {msg:100}
+        },
+        methods:{
+            add(){
+                this.msg++;
+            }
+        }
+    }
+</script>
+<style>
+    div{
+        width:100px;
+        height:200px;
+        background: red;
+    }
+</style>
